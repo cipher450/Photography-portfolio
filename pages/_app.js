54ef4d08ce804f94fad99759bb46b1ae51f666cc
Cipher450 <hadjmus99@gmail.com>
2023-02-02 02:08:52 -1200

    Fixed rel="noreferrer"

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,7 @@ export default function App({ Component, pageProps }) {
       <footer>
         <h1>
           Coded & Designed by{" "}
-          <a href="https://cipher450.github.io/Personal-website/" target="_blank">Mustapha</a>
+          <a href="https://cipher450.github.io/Personal-website/" target="_blank" rel="noreferrer">Mustapha</a>
         </h1>
       </footer>
     </>
